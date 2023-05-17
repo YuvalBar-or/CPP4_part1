4ac4f921231a3c8e6f5fb25bcd151f6e90806ebd
YuvalBar-or <yuvalb.mail@gmail.com>
2023-05-17 18:11:16 +0300

    Update Cowboy.cpp

--- sources/Cowboy.cpp
+++ sources/Cowboy.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace ariel; 
 
 
-Cowboy :: Cowboy (string name, const Point &location):Character(name, location, 110),bullets(60){}
+Cowboy :: Cowboy (string name, const Point &location):Character(name, location, 110),bullets(6){}
 void Cowboy:: shoot(Character *enemy){}
 bool Cowboy :: hasboolets(){return false;}
 void Cowboy :: reload(){}
